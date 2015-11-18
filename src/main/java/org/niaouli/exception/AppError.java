15b504504b1bfb9a83699c242a000650e3e1c471
Arnaud Rolly <github@gedial.com>
2015-11-18 21:40:19 +0100

    Initial source code import

---
+++ src/main/java/org/niaouli/exception/AppError.java
@@ -0,0 +1,153 @@
+/**
+ * This file is part of Niaouli Exception.
+ *
+ * Niaouli Exception is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * Niaouli Exception is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * Niaouli Exception. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.niaouli.exception;
+
+import java.io.Serializable;
+import java.util.Arrays;
+
+/**
+ * Error description.
+ *
+ * @author Arnaud Rolly <arnaud@niaouli.org>
+ */
+public class AppError implements Serializable {
+
+    /**
+     * Message to describe the error.
+     */
+    private final String msg;
+    /**
+     * Message parameters.
+     */
+    private final Serializable[] params;
+    /**
+     * Field that caused the error.
+     */
+    private final String field;
+
+    /**
+     * Create a new error.
+     *
+     * @param pMsg Message.
+     */
+    public AppError(final String pMsg) {
+        this(pMsg, null, null);
+    }
+
+    /**
+     * Create a new error.
+     *
+     * @param pMsg Message.
+     * @param pParams Message parameters.
+     */
+    public AppError(final String pMsg, final Serializable[] pParams) {
+        this(pMsg, pParams, null);
+    }
+
+    /**
+     * Create a new error.
+     *
+     * @param pMsg Message.
+     * @param pField Field that caused the error.
+     */
+    public AppError(final String pMsg, final String pField) {
+        this(pMsg, null, pField);
+    }
+
+    /**
+     * Create a new error.
+     *
+     * @param pMsg Message.
+     * @param pParams Message parameters.
+     * @param pField Field that caused the error.
+     */
+    public AppError(final String pMsg, final Serializable[] pParams, final String pField) {
+        this.msg = pMsg;
+        this.params = pParams;
+        this.field = pField;
+    }
+
+    /**
+     * Returns the error message.
+     *
+     * @return Error message.
+     */
+    public final String getMsg() {
+        return msg;
+    }
+
+    /**
+     * Returns the error message parameters.
+     *
+     * @return Error message parameters.
+     */
+    public final Serializable[] getParams() {
+        return params;
+    }
+
+    /**
+     * Return the field that caused the error.
+     *
+     * @return Field that caused the error.
+     */
+    public final String getField() {
+        return field;
+    }
+
+    @Override
+    public final int hashCode() {
+        int hash = 3;
+        hash = 11 * hash + (this.msg != null ? this.msg.hashCode() : 0);
+        hash = 11 * hash + Arrays.deepHashCode(this.params);
+        hash = 11 * hash + (this.field != null ? this.field.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public final boolean equals(Object pObj) {
+        if (pObj == null) {
+            return false;
+        }
+        if (getClass() != pObj.getClass()) {
+            return false;
+        }
+        final AppError other = (AppError) pObj;
+        if (this.msg == null ? other.msg != null : !this.msg.equals(other.msg)) {
+            return false;
+        }
+        if (!Arrays.deepEquals(this.params, other.params)) {
+            return false;
+        }
+        if (this.field == null ? other.field != null : !this.field.equals(other.field)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("ServiceError[msg=");
+        sb.append(msg);
+        sb.append(",params=");
+        sb.append(params);
+        sb.append(",field=");
+        sb.append(field);
+        sb.append("]");
+        return sb.toString();
+    }
+}
---
+++ src/main/java/org/niaouli/exception/AppException.java
@@ -0,0 +1,102 @@
+/**
+ * This file is part of Niaouli Exception.
+ *
+ * Niaouli Exception is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * Niaouli Exception is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * Niaouli Exception. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.niaouli.exception;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Exception with embedded errors descriptions.
+ *
+ * @author Arnaud Rolly <arnaud@niaouli.org>
+ */
+public class AppException extends Throwable {
+
+    /**
+     * Errors that caused the exception.
+     */
+    private final List<AppError> errors = new ArrayList<AppError>();
+
+    /**
+     * Creates a new exception.
+     *
+     * @param pErrors Errors that caused the exception.
+     */
+    public AppException(final List<AppError> pErrors) {
+        this.errors.addAll(pErrors);
+    }
+
+    /**
+     * Creates a new exception with one error.
+     *
+     * @param pError Error that caused the exception.
+     */
+    public AppException(final AppError pError) {
+        this.errors.add(pError);
+    }
+
+    /**
+     * Creates a new exception with one error.
+     *
+     * @param pMsg Message of the error.
+     */
+    public AppException(final String pMsg) {
+        this(pMsg, null, null);
+    }
+
+    /**
+     * Creates a new exception with one error.
+     *
+     * @param pMsg Message of the error.
+     * @param pParams Parameters of the message.
+     */
+    public AppException(final String pMsg, final Serializable[] pParams) {
+        this(pMsg, pParams, null);
+    }
+
+    /**
+     * Creates a new exception with one error.
+     *
+     * @param pMsg Message of the error.
+     * @param pField Field of the error.
+     */
+    public AppException(final String pMsg, final String pField) {
+        this(pMsg, null, pField);
+    }
+
+    /**
+     * Creates a new exception with one error.
+     *
+     * @param pMsg Message of the error.
+     * @param pParams Parameters of the message.
+     * @param pField Field of the error.
+     */
+    public AppException(final String pMsg, final Serializable[] pParams, final String pField) {
+        this.errors.add(new AppError(pMsg, pParams, pField));
+    }
+
+    /**
+     * Returns the errors attached to the exception.
+     *
+     * @return Errors attached to the exception.
+     */
+    public final List<AppError> getErrors() {
+        return Collections.unmodifiableList(errors);
+    }
+}
